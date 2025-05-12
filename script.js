0b0ecf9c259910504e0543e445ec6439d123aee0
SANJAR326 <sagynbekkyzymeerim9@gmail.com>
2025-05-12 19:52:16 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let a, b;
+let score = 0
+
+const question = document.getElementById("question")
+const answerInput = document.querySelector("input")
+const checkBtn = document.getElementById("checkBtn")
+const result = document.querySelector("result")
+    
+    function newQuestion(){
+
+    }
